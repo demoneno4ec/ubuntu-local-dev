639cdc9ccc86e0f335814bfbf63b16e918d966af
demoneno4ec <ddemoneno4ec@gmail.com>
2024-03-25 22:02:19 +0300

    Правки после 1 запуска
    - Убрать лишние амперсанды у устанавливаемых программ
    - Убрать лишние амперсанды в конфигах
    - Дополнить инструкцию для установки

--- init.sh
+++ init.sh
@@ -8,40 +8,42 @@ Cyan='\033[0;36m'  # Cyan
 # Сбор пользовательских настроек
 echo "# Hello, please introduce yourself."
 
-echo -en "${Cyan} ## Your name (${SUDO_USER}): "
+echo -en "${Cyan} ## Your name (${SUDO_USER}): ${NC}"
 read -r name
 
-echo -en "${Cyan} # Your email: "
+echo -en "${Cyan} # Your email: ${NC}"
 read -r email
 
-echo "${name} ${email}"
-
 # Первоначальное обновление зависимостей и пакетов
-printf "${Green}# Обновление зависимостей \n"
+printf "${Green}# Обновление зависимостей \n${NC}"
 apt update && \
 	apt upgrade -y
 
 # установка приложений (apt)
-printf "${Green}# Установка приложений (apt) \n"
+printf "${Green}# Установка приложений (apt) \n${NC}"
 apt install -y \
-	git && \
-	vim && \
-	curl && \
+	git \
+	vim \
+	curl \
 	gnome-tweaks
 
 # Установка приложений не из apt
-printf "${Green}# Установка приложений (not apt) \n"
+printf "${Green}# Установка приложений (not apt) \n${NC}"
 ## Установка браузера Google Chrome
-printf "${Green}## Установка Google Chrome \n"
+printf "${Green}## Установка Google Chrome \n${NC}"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Конфигурирование приложений
-printf "${Green}# Конфигурирование приложений \n"
+printf "${Green}# Конфигурирование приложений \n${NC}"
 ## git
-printf "${Green}## git \n"
-git config --global user.email $email && \
-git config --global user.name $name && \
-git config --global core.editor 'vim' && \
+printf "${Green}## git \n${NC}"
+git config --global user.email $email
+git config --global user.name $name
+git config --global core.editor 'vim'
 ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ''
+git config --global --list
 
+## Права доступы
+### TODO Можем быть можно переделать на ${SUDO_USER}
+chown -R demoneno4ec:demoneno4ec ~/projects
--- readme.md
+++ readme.md
@@ -16,7 +16,12 @@ TODO
 
 # Инструкция
 ```sh
+sudo apt install -y git
 mkdir -p ~/projects/demoneno4ec
 sudo git clone https://github.com/demoneno4ec/ubuntu-local-dev.git ~/projects/demoneno4ec/ubuntu-local-dev
 sudo ~/projects/demoneno4ec/ubuntu-local-dev/init.sh
 ```
+
+# vm machine
+- устройства -> Подключить образ диска Дополнительной гостевой ОС
+- /media/demoneno4ec/VBox_GAs_7.0.14/autorun.sh
